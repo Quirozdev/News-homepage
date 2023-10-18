5bd81c80342b3ddc2ab2ea90b27eef1c29d38917
Quirozdev <luisdevv232@gmail.com>
2023-10-17 21:44:28 -0700

    add basic project structure with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex content-center justify-center h-screen">
+      <div className="grid w-full h-full grid-rows-[100px_1fr_200px] max-w-7xl">
+        <div className="bg-[red]"></div>
+        <div className="bg-[blue]"></div>
+        <div className="bg-[green]"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
